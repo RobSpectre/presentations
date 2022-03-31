1d1a54e708fcbdf7d25dae8e76034be6358fc1b2
Rob Spectre <rob@brooklynhacker.com>
2022-03-31 16:18:30 -0400

    1) Added `coverage` to `.gitignore`. 2) Added coverage to jest config. 3) Fixed `About.vue` view.

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,21 @@ module.exports = {
   preset: '@vue/cli-plugin-unit-jest',
   setupFiles: [
     './tests/setupTests.js'
-  ]
+  ],
+  collectCoverage: true,
+  collectCoverageFrom: [
+    'src/**/*.{js,vue}',
+    '!**/node_modules/**'
+  ],
+  coverageReporters: [
+    'html',
+    'text-summary'
+  ],
+  globals: {
+    'vue-jest': {
+      pug: {
+        doctype: 'html'
+      }
+    }
+  }
 }
