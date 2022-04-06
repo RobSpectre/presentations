d5f1c3f4403e3fbc4352504a60997e5d8dd997e1
Rob Spectre <rob@brooklynhacker.com>
2022-04-06 14:12:12 -0400

    1) Factored out a more reusable `GameContent` slide for the setup for a round. 2) Integrated `GameContent` with `ItemIntro` for Which Is Which.

--- src/components/WhichIsWhich/ItemIntro.vue
+++ src/components/WhichIsWhich/ItemIntro.vue
@@ -1,26 +1,28 @@
 <template lang='pug'>
 GameSlide
   PlayersSidebar(:players='players')
-  .flex.flex-grow.overflow-y-auto(class='focus:outline-none' tabindex='0')
-    .flex-col.items-center.w-full
-      .flex-none.bg-blue
-        h3.text-white.fragment(v-for='hint in item.hints') {{ hint }}
-      .flex.flex-initial.items-center.justify-center
-          img.fragment(:src='item.image')
-    h1.fragment {{ item.name }}
+  GameContent
+    template(v-slot:header)
+      h3.text-white.fragment(v-for='hint in item.hints') {{ hint }}
+    template(v-slot:content)
+      img.fragment(:src='item.image')
+    template(v-slot:footer)
+      h1.fragment {{ item.name }}
 </template>
 
 <script>
 import { mapGetters } from 'vuex'
 
 import GameSlide from '@/components/base/GameSlide.vue'
+import GameContent from '@/components/base/GameContent.vue'
 import PlayersSidebar from '@/components/Players/PlayersSidebar.vue'
 
 export default {
   name: 'ItemIntro',
   components: {
     GameSlide,
-    PlayersSidebar
+    PlayersSidebar,
+    GameContent
   },
   props: [
     'item'
---
+++ src/components/base/GameContent.vue
@@ -0,0 +1,15 @@
+<template lang='pug'>
+.flex.flex-grow.overflow-y-auto(class='focus:outline-none' tabindex='0')
+  .flex-col.items-center.w-full
+    .flex-none.bg-blue
+      slot(name='header')
+    .flex.flex-initial.items-center.justify-center
+      slot(name='content')
+      slot(name='footer')
+</template>
+
+<script>
+export default {
+  name: 'GameContent'
+}
+</script>
