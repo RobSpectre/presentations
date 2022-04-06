307827408e911392e7bbbe49b3fc0e9129915ed4
Rob Spectre <rob@brooklynhacker.com>
2022-04-06 14:39:04 -0400

    1) Integrated `GameContent` into `CelebrityIntro`. 2) Integrated `GameContent` into `CelebrityVideo`.

--- src/components/CameoPriceIsRight/CelebrityIntro.vue
+++ src/components/CameoPriceIsRight/CelebrityIntro.vue
@@ -1,28 +1,30 @@
 <template lang='pug'>
-section
-  .h-screen.flex.overflow-hidden
-    PlayersSidebar(:players='players')
-    main.flex-1.relative.z-0.overflow-y-auto.py-6(class='focus:outline-none' tabindex='0')
-      .max-w-7xl.mx-auto.px-4(class='sm:px-6 md:px-8')
-        .py-4.grid.grid-rows-3.grid-flow-col.gap-4
-          .row-span-1.col-span-2.border-4.border-dashed.border-gray-200.rounded-lg.w-.cluse
-            h3.fragment {{ hint1 }}
-            h3.fragment {{ hint2 }}
-            h3.fragment {{ hint3 }}
-          .row-span-2.col-span-2
-            img.fragment.m-0(style='margin: auto;' :src='image')
-    h1.fragment {{ celebrity }}
+GameSlide
+  PlayersSidebar(:players='players')
+  GameContent
+    template(v-slot:header)
+      h3.fragment.text-white {{ hint1 }}
+      h3.fragment.text-white {{ hint2 }}
+      h3.fragment.text-white {{ hint3 }}
+    template(v-slot:content)
+      img.fragment(:src='image')
+    template(v-slot:footer)
+      h1.fragment {{ celebrity }}
 </template>
 
 <script>
 import { mapGetters } from 'vuex'
 
 import PlayersSidebar from '@/components/Players/PlayersSidebar.vue'
+import GameSlide from '@/components/base/GameSlide.vue'
+import GameContent from '@/components/base/GameContent.vue'
 
 export default {
   name: 'CelebrityIntro',
   components: {
-    PlayersSidebar
+    PlayersSidebar,
+    GameSlide,
+    GameContent
   },
   props: [
     'celebrity',
--- src/components/CameoPriceIsRight/CelebrityVideo.vue
+++ src/components/CameoPriceIsRight/CelebrityVideo.vue
@@ -1,24 +1,27 @@
 <template lang="pug">
-section
-  .h-screen.flex.overflow-hidden
-    PlayersSidebar(:players='players')
-    main.flex-1.relative.z-0.overflow-y-auto.py-6(class='focus:outline-none' tabindex='0')
-      .max-w-7xl.mx-auto.px-4(class='sm:px-6 md:px-8')
-        .py-4
-          video.mx-auto(controls='' style='height: 1200px;')
-            source(:src='video' type='video/mp4')
-  h1 {{ celebrity }}
+GameSlide
+  PlayersSidebar(:players='players')
+  GameContent
+    template(v-slot:content)
+      video.mx-auto(controls='' style='height: 1200px;')
+        source(:src='video' type='video/mp4')
+    template(v-slot:footer)
+      h1 {{ celebrity }}
 </template>
 
 <script>
 import { mapGetters } from 'vuex'
 
+import GameSlide from '@/components/base/GameSlide.vue'
+import GameContent from '@/components/base/GameContent.vue'
 import PlayersSidebar from '@/components/Players/PlayersSidebar.vue'
 
 export default {
   name: 'CelebrityVideo',
   components: {
-    PlayersSidebar
+    PlayersSidebar,
+    GameSlide,
+    GameContent
   },
   props: [
     'celebrity',
