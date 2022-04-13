08a80734d657167083518ad6ed1f5a57f008aa76
Rob Spectre <rob@brooklynhacker.com>
2022-04-13 19:56:32 -0400

    1) fix indent on h1 for `CelebrityGuess`

--- src/components/CameoPriceIsRight/CelebrityGuess.vue
+++ src/components/CameoPriceIsRight/CelebrityGuess.vue
@@ -20,7 +20,7 @@ GameSlide
         | Is The Price Right?&#x9;
         svg.ml-3.-mr-1.h-5.w-5(fill='currentColor' viewbox='0 0 20 20')
           path(d='M12 22a10 10 0 1 1 0-20 10 10 0 0 1 0 20zm0-2a8 8 0 1 0 0-16 8 8 0 0 0 0 16zm1-11v2h1a3 3 0 0 1 0 6h-1v1a1 1 0 0 1-2 0v-1H8a1 1 0 0 1 0-2h3v-2h-1a3 3 0 0 1 0-6h1V6a1 1 0 0 1 2 0v1h3a1 1 0 0 1 0 2h-3zm-2 0h-1a1 1 0 1 0 0 2h1V9zm2 6h1a1 1 0 0 0 0-2h-1v2z')
-h1 {{ celebrity }}
+  h1 {{ celebrity }}
   WinnerCard(
     v-if='winner.length > 0'
     :winners='[winner]'
