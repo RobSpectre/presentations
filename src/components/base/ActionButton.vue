6a80076c37948bb4c1244d3dcee8ec4362b7ed96
Rob Spectre <rob@brooklynhacker.com>
2022-04-14 12:18:24 -0400

    1) Added `ActionButton` component.

---
+++ src/components/base/ActionButton.vue
@@ -0,0 +1,26 @@
+<template lang='pug'>
+button.actionButton(
+  @click='click'
+) {{ label }}
+
+</template>
+
+<script>
+export default {
+  name: 'ActionButton',
+  props: {
+    label: String
+  },
+  methods: {
+    click () {
+      this.$emit('clicked')
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.actionButton {
+  @apply inline-flex justify-center px-6 py-3 border border-transparent leading-6 font-medium rounded-md text-white text-2xl uppercase bg-darkgray transition ease-in-out duration-150 my-2 hover:bg-slate-300 focus:outline-none focus:border-slate-300 active:bg-slate-300;
+}
+</style>
